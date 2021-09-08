fd3e958e304cabdabd0723182f68e46612c73819
William Verde <williamverde10@gmail.com>
2021-09-08 16:46:05 -0400

    Added: permissions restore

--- Config/permissions.php
+++ Config/permissions.php
@@ -7,6 +7,7 @@ return [
         'edit' => 'ibooking::categories.edit resource',
         'destroy' => 'ibooking::categories.destroy resource',
         'manage' => 'ibooking::categories.manage resource',
+        'restore' => 'ibooking::categories.restore resource',
     ],
     'ibooking.services' => [
         'index' => 'ibooking::services.list resource',
@@ -14,6 +15,7 @@ return [
         'edit' => 'ibooking::services.edit resource',
         'destroy' => 'ibooking::services.destroy resource',
         'manage' => 'ibooking::services.manage resource',
+        'restore' => 'ibooking::services.restore resource',
     ],
     'ibooking.resources' => [
         'index' => 'ibooking::resources.list resource',
@@ -21,6 +23,7 @@ return [
         'edit' => 'ibooking::resources.edit resource',
         'destroy' => 'ibooking::resources.destroy resource',
         'manage' => 'ibooking::resources.manage resource',
+        'restore' => 'ibooking::resources.restore resource',
     ],
     'ibooking.reservations' => [
         'index' => 'ibooking::reservations.list resource',
@@ -28,6 +31,7 @@ return [
         'edit' => 'ibooking::reservations.edit resource',
         'destroy' => 'ibooking::reservations.destroy resource',
         'manage' => 'ibooking::reservations.manage resource',
+        'restore' => 'ibooking::reservations.restore resource',
     ],
     'ibooking.reservationitems' => [
         'index' => 'ibooking::reservationitems.list resource',
@@ -35,6 +39,7 @@ return [
         'edit' => 'ibooking::reservationitems.edit resource',
         'destroy' => 'ibooking::reservationitems.destroy resource',
         'manage' => 'ibooking::reservationitems.manage resource',
+        'restore' => 'ibooking::reservationitems.restore resource',
     ],
 // append
 
