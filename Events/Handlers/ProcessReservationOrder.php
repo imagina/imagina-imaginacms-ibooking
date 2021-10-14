6d961ce37b4c0f7d4b3d014f825e3c8e6fec146a
William Verde <williamverde10@gmail.com>
2021-10-14 09:10:03 -0400

    Update: event process reservation order

--- Events/Handlers/ProcessReservationOrder.php
+++ Events/Handlers/ProcessReservationOrder.php
@@ -14,17 +14,20 @@ class ProcessReservationOrder
 
     public function handle($event)
     {
+
+        \Log::info('Ibooking: Event - Process Reservation');
+
         $order = $event->order;
         //Order is Proccesed
         if($order->status_id==13){
 
             $reservationData = ['customer_id' => $order->customer_id,'items' => []];
 
+            \Log::info('Ibooking: Event - Data: '.json_encode($reservationData));
 
             foreach($order->orderItems as $item){
-
                 // Reservation Data
-                $reservationData['items'][] = json_decode($item->options)->reservationItemData;
+                $reservationData['items'][] = (array)$item->options->reservationItemData;
 
                 $reservationRepository = app('Modules\Ibooking\Repositories\ReservationRepository');
 
@@ -34,7 +37,7 @@ class ProcessReservationOrder
                 // Log
                 $user = $order->customer;
                 \Log::info("{$this->logtitle}Order Completed | Register reservation to user ID {$user->id}");
-               
+
             }
 
         }// end If
