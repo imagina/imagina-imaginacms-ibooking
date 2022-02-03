a87ae1dda79e53e0c12f86ec713e97299c872ce1
Michael <msolano@imaginacolombia.com>
2022-02-03 12:36:50 -0500

    Add: CMS Translations

---
+++ Resources/lang/en/cms.php
@@ -0,0 +1,34 @@
+<?php
+return [
+  "newCategory" => 'New Category',
+  "updateCategory" => 'Update Category',
+  "newService" => 'New Service',
+  "updateService" => 'Update Service',
+  "newResource" => 'New Resource',
+  "updateResource" => 'Update Resource',
+  "newReservation" => 'New Reservation',
+  "updateReservation" => 'Update Reservation',
+
+  "titleStepCategory" => 'What are you looking for?',
+  "descriptionStepCategory" => 'Choose the category of your interest',
+  "titleStepService" => 'Which service do you want?',
+  "descriptionStepService" => 'Choose between all our services',
+  "titleStepResource" => 'Do you have a favorite?',
+  "descriptionStepResource" => 'Choose the one you prefer the most',
+  "titleStepDate" => 'When do you want your reservation?',
+  "descriptionStepDate" => 'Choose the date and time',
+  "titleStepAvailability" => 'Select your reservation!',
+  "descriptionStepAvailability" => 'Select the reservation of your preference',
+
+  "chooseDateReservation" => 'Choose the day for your reservation',
+  "chooseTimeReservation" => 'Choose the time for your reservation',
+
+  "myResource" => 'My Resource',
+  "message" => [
+    "meet" => [
+      "notFound" => 'Enter your Zoom account (email) to link and create meetings ...',
+      "pending" => 'Almost there! Check your email and accept the link for your Zoom account ...',
+      "success" => 'Everything is ready! We take care of creating Zoom meetings for you ...',
+    ]
+  ]
+];
