6c7e260a2a8c05db610b52e5d7183c07bb20dee1
msolano <msolano@imaginacolombia.com>
2022-08-13 14:23:47 -0500

    +Update: Not add `redirectTo` parameter in response after create a reservation if it's public and not have pay

--- Http/Controllers/Api/ReservationApiController.php
+++ Http/Controllers/Api/ReservationApiController.php
@@ -7,6 +7,7 @@ use Modules\Core\Icrud\Controllers\BaseCrudController;
 //Model Repository
 use Modules\Ibooking\Repositories\ReservationRepository;
 use Modules\Ibooking\Entities\Reservation;
+use Modules\Ibooking\Transformers\ReservationTransformer;
 
 use Illuminate\Http\Request;
 
@@ -35,13 +36,16 @@ class ReservationApiController extends BaseCrudController
   {
     \DB::beginTransaction();
     try {
+      //Get user authenticated
+      $userData = \Auth::user();
+
       //Check if allow public reservations
-      if(!setting("ibooking::allowPublicReservation") && !\Auth::user()){
+      if (!setting("ibooking::allowPublicReservation") && !$userData) {
         $status = 500;
         $response = ["messages" => [
           [
-          "message" => trans('ibooking::common.noAllowPublicReservations'),
-          "type" => "error"
+            "message" => trans('ibooking::common.noAllowPublicReservations'),
+            "type" => "error"
           ]
         ]];
       }else {
@@ -53,9 +57,7 @@ class ReservationApiController extends BaseCrudController
         if (isset($this->model->requestValidation['create'])) {
           $this->validateRequestApi(new $this->model->requestValidation['create']($modelData));
         }
-
-        //\Log::info("Ibooking: ReservationApiController|Create|ModelDataRequest: ".json_encode($modelData));
-
+        //Create reservations
         $reservation = $this->reservationService->createReservation($modelData);
 
         /*
@@ -71,29 +73,29 @@ class ReservationApiController extends BaseCrudController
         if (is_module_enabled('Icommerce') && setting('ibooking::reservationWithPayment', null, false)) {
 
           $checkoutCart = $this->reservationService->createCheckoutCart($modelData, $reservation);
-       
+
           $response = ["data" => ["redirectTo" => url(trans("icommerce::routes.store.checkout.create"))]];
         } else {
 
-          //Default 
+          //Default
           $url = "/ipanel/#/booking/reservations/index";
 
           //If it's iadmin take that url
           if(isset($params->setting) && isset($params->setting->appMode)) {
             $url = "/".$params->setting->appMode."/#/booking/reservations/index";
           }
-
-          $response = ["data" => ["redirectTo" => url($url)]];
-
+          //Response
+          $response = ["data" => [
+            "redirectTo" => $userData ? url($url) : null,
+            "reservation" => new ReservationTransformer($reservation)
+          ]];
         }
 
 
         \DB::commit(); //Commit to Data Base
       }
     } catch (\Exception $e) {
-
-      \Log::error('Ibooking: ReservationApiController|Create|Message: '.$e->getMessage().' | FILE: '.$e->getFile().' | LINE: '.$e->getLine());
-
+      \Log::error('[error-ibooking] ReservationApiController|Create|Message: ' . $e->getMessage() . ' | FILE: ' . $e->getFile() . ' | LINE: ' . $e->getLine());
       \DB::rollback();//Rollback to Data Base
       $status = $this->getStatusError($e->getCode());
       $response = ["errors" => $e->getMessage()];
