47d4eeb3757a7be46f2c75dc42690cf52de84d11
michael <msolano2505@gmail.com>
2024-09-15 18:30:08 -0500

    Updated Status entity as a CrudStaticModel

--- Entities/Status.php
+++ Entities/Status.php
@@ -2,55 +2,62 @@
 
 namespace Modules\Ibooking\Entities;
 
-class Status
+use Modules\Core\Icrud\Entities\CrudStaticModel;
+
+class Status extends CrudStaticModel
 {
-    const PENDING = 0;
+  const PENDING = 0;
+  const APPROVED = 1;
+  const CANCELED = 2;
+  const INPROGRESS = 3;
+  const COMPLETED = 4;
 
-    const APPROVED = 1;
+  public function __construct()
+  {
+    $this->records = [
+      self::PENDING => [
+        'title' => trans('ibooking::reservations.status.pending'),
+        'color' => ''
+      ],
+      self::APPROVED => [
+        'title' => trans('ibooking::reservations.status.approved'),
+        'color' => ''
+      ],
+      self::CANCELED => [
+        'title' => trans('ibooking::reservations.status.canceled'),
+        'color' => ''
+      ],
+      self::INPROGRESS => [
+        'title' => trans('ibooking::reservations.status.inProgress'),
+        'color' => ''
+      ],
+      self::COMPLETED => [
+        'title' => trans('ibooking::reservations.status.completed'),
+        'color' => ''
+      ],
+    ];
+  }
 
-    const CANCELED = 2;
+  public function convertToSettings()
+  {
+    $statuses = $this->records;
+    $statusSetting = [];
+    foreach ($statuses as $key => $status) {
+      array_push($statusSetting, [
+        'label' => $status['title'],
+        'value' => $key
+      ]);
+    }
+    //\Log::info("StatusSetting: ".json_encode($statusSetting));
+    return $statusSetting;
+  }
 
-    const INPROGRESS  = 3;
-
-    const COMPLETED = 4;
-
-    private $statuses = [];
-
-    private $statuses2 = [];
-
-    public function __construct()
-    {
-        $this->statuses = [
-            self::PENDING => trans('ibooking::reservations.status.pending'),
-            self::APPROVED => trans('ibooking::reservations.status.approved'),
-            self::CANCELED => trans('ibooking::reservations.status.canceled'),
-            self::INPROGRESS => trans('ibooking::reservations.status.inProgress'),
-            self::COMPLETED => trans('ibooking::reservations.status.completed'),
-        ];
+  public function get($statusId)
+  {
+    if (isset($this->records[$statusId])) {
+      return $this->records[$statusId]['title'];
     }
 
-    public function lists()
-    {
-        return $this->statuses;
-    }
-
-    public function convertToSettings()
-    {
-        $statuses = $this->statuses;
-        $statusSetting = [];
-        foreach ($statuses as $key => $status) {
-            array_push($statusSetting, ['label' => $status, 'value' => $key]);
-        }
-        //\Log::info("StatusSetting: ".json_encode($statusSetting));
-        return $statusSetting;
-    }
-
-    public function get($statusId)
-    {
-        if (isset($this->statuses[$statusId])) {
-            return $this->statuses[$statusId];
-        }
-
-        return $this->statuses[self::PENDING];
-    }
+    return $this->records[self::PENDING]['title'];
+  }
 }
--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -3,41 +3,47 @@
 use Illuminate\Routing\Router;
 
 Route::prefix('/ibooking/v1')->group(function (Router $router) {
-    $router->apiCrud([
-        'module' => 'ibooking',
-        'prefix' => 'categories',
-        'controller' => 'CategoryApiController',
-        'middleware' => ['index' => []], // Just Testing
-    ]);
-    $router->apiCrud([
-        'module' => 'ibooking',
-        'prefix' => 'services',
-        'controller' => 'ServiceApiController',
-        'middleware' => ['index' => []], // Just Testing
-    ]);
-    $router->apiCrud([
-        'module' => 'ibooking',
-        'prefix' => 'resources',
-        'controller' => 'ResourceApiController',
-        'middleware' => ['index' => [], 'show' => []], // Just Testing
-    ]);
-    $router->apiCrud([
-        'module' => 'ibooking',
-        'prefix' => 'reservations',
-        'controller' => 'ReservationApiController',
-        'middleware' => ['create' => ['optional-auth']], // Just Testing
-    ]);
-    $router->apiCrud([
-        'module' => 'ibooking',
-        'prefix' => 'reservation-items',
-        'controller' => 'ReservationItemApiController',
-        'permission' => 'ibooking.reservationitems'
-        //'middleware' => ['create' => [],'index' => [],'delete' => []] // Just Testing
-    ]);
-    $router->get('availabilities', [
-        'as' => 'api.ibooking.availability',
-        'uses' => 'AvailabilityApiController@availability',
-    ]);
+  $router->apiCrud([
+    'module' => 'ibooking',
+    'prefix' => 'categories',
+    'controller' => 'CategoryApiController',
+    'middleware' => ['index' => []], // Just Testing
+  ]);
+  $router->apiCrud([
+    'module' => 'ibooking',
+    'prefix' => 'services',
+    'controller' => 'ServiceApiController',
+    'middleware' => ['index' => []], // Just Testing
+  ]);
+  $router->apiCrud([
+    'module' => 'ibooking',
+    'prefix' => 'resources',
+    'controller' => 'ResourceApiController',
+    'middleware' => ['index' => [], 'show' => []], // Just Testing
+  ]);
+  $router->apiCrud([
+    'module' => 'ibooking',
+    'prefix' => 'reservations',
+    'controller' => 'ReservationApiController',
+    'middleware' => ['create' => ['optional-auth']], // Just Testing
+  ]);
+  $router->apiCrud([
+    'module' => 'ibooking',
+    'prefix' => 'reservation-items',
+    'controller' => 'ReservationItemApiController',
+    'permission' => 'ibooking.reservationitems'
+    //'middleware' => ['create' => [],'index' => [],'delete' => []] // Just Testing
+  ]);
+  $router->get('availabilities', [
+    'as' => 'api.ibooking.availability',
+    'uses' => 'AvailabilityApiController@availability',
+  ]);
+  $router->apiCrud([
+    'module' => 'iwebhooks',
+    'prefix' => 'statuses',
+    'staticEntity' => 'Modules\Ibooking\Entities\Status',
+    //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []]
+  ]);
 
-    // append
+  // append
 });
