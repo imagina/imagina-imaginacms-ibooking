744a380b25e7b10f8cb92d24112c1037fce803ab
michael <msolano2505@gmail.com>
2024-09-15 18:02:10 -0500

    added filter of resourceId and serviceId to Reservation

--- Repositories/Eloquent/EloquentReservationRepository.php
+++ Repositories/Eloquent/EloquentReservationRepository.php
@@ -27,7 +27,23 @@ class EloquentReservationRepository extends EloquentCrudRepository implements Re
      * Example filter Query
      * if (isset($filter->status)) $query->where('status', $filter->status);
      */
+    if (isset($filter->resourceId)) {
+      $resorceId = is_array($filter->resourceId) ? $filter->resourceId : [$filter->resourceId];
+      if (count($resorceId)) {
+        $query->whereHas('items', function ($query) use ($resorceId) {
+          $query->whereIn('resource_id', $resorceId);
+        });
+      }
+    }
 
+    if (isset($filter->serviceId)) {
+      $serviceId = is_array($filter->serviceId) ? $filter->serviceId : [$filter->serviceId];
+      if (count($serviceId)) {
+        $query->whereHas('items', function ($query) use ($serviceId) {
+          $query->whereIn('service_id', $serviceId);
+        });
+      }
+    }
 
     //Response
     return $query;
